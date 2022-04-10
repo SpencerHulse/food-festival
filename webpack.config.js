d13dfe115113d62205656e79724e98ce1e80ca98
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-09 21:26:17 -0400

    Changed webpack publicPath to an empty string.

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: `${__dirname}/dist`,
+    publicPath: "",
   },
   module: {
     // Regex for ending in webp while being case-insensitive
